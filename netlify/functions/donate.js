3c856f90c723acb467d10cc69a918aa42e7eea55
HenokTG <hena143877tg@gmail.com>
2022-07-24 19:17:22 +0300

    HulugramFund App Integration

--- netlify/functions/donate.js
+++ netlify/functions/donate.js
@@ -1,14 +1,31 @@
 const axios = require("axios").default;
 const { Telegraf } = require("telegraf");
 
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const webLink = process.env.APP_URL; 
+
+const bot = new Telegraf(BOT_TOKEN);
+
 exports.handler = async (event) => {
-	console.log("Received an update from Telegram!", process.env.APP_URL);
+	console.log("Received an update from Telegram!", webLink);
     await axios.post(
-		`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+		`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
 		{
 			chat_id: JSON.parse(event.body).message.chat.id,
 			text: "I got your message!",
 		}
 	);
+
+    bot.start((ctx) =>
+		ctx.reply("Welcome to Hulugram Fund", {
+			reply_markup: {
+				inline_keyboard: [
+					[{ text: "Hulu Donate", web_app: { url: webLink } }],
+				],
+			},
+		})
+	);
+
+	bot.launch();
 	return { statusCode: 200 };
 };
