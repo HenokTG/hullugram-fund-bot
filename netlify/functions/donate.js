8d9ba16e754ee97ba923e7612a06efa06bf81e47
HenokTG <hena143877tg@gmail.com>
2022-07-24 20:20:05 +0300

    Bemilew

--- netlify/functions/donate.js
+++ netlify/functions/donate.js
@@ -1,35 +1,32 @@
-const axios = require("axios").default;
-// const { Telegraf } = require("telegraf");
+// const axios = require("axios").default;
+const { Telegraf } = require("telegraf");
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const webLink = process.env.APP_URL;
 
-// const bot = new Telegraf(BOT_TOKEN);
+const bot = new Telegraf(BOT_TOKEN);
 
-exports.handler = async (event) => {
-	console.log("Received an update from Telegram!", BOT_TOKEN);
-	
-    await axios.post(
-		`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-		{
-			chat_id: JSON.parse(event.body).message.chat.id,
-			text: "I got your message!",
-		}
-	);
-	
+bot.start((ctx) =>
+	ctx.reply("Welcome to Hulugram Fund", {
+		reply_markup: {
+			inline_keyboard: [
+				[{ text: "Hulu Donate", web_app: { url: webLink } }],
+			],
+		},
+	})
+);
+bot.launch();
 
-	// bot.start((ctx) =>
-	// 	ctx.reply("Welcome to Hulugram Fund", {
-	// 		reply_markup: {
-	// 			inline_keyboard: [
-	// 				[{ text: "Hulu Donate", web_app: { url: webLink } }],
-	// 			],
-	// 		},
-	// 	})
-	// );
-	// bot.launch();
+// exports.handler = async (event) => {
+// 	console.log("Received an update from Telegram!", event.body);
 
-    console.log("Received an update from Telegram!", webLink);
-	
-    return { statusCode: 200 };
-};
+//     await axios.post(
+// 		`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+// 		{
+// 			chat_id: JSON.parse(event.body).message.chat.id,
+// 			text: "I got your message!",
+// 		}
+// 	);
+
+//     return { statusCode: 200 };
+// };
