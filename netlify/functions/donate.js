4c24426f637c3e59a19c48c43f1e70deab68d187
HenokTG <hena143877tg@gmail.com>
2022-07-24 19:42:36 +0300

    cnc

--- netlify/functions/donate.js
+++ netlify/functions/donate.js
@@ -8,24 +8,24 @@ const bot = new Telegraf(BOT_TOKEN);
 
 exports.handler = async (event) => {
 	console.log("Received an update from Telegram!", webLink);
-	// await axios.post(
-	// 	`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
-	// 	{
-	// 		chat_id: JSON.parse(event.body).message.chat.id,
-	// 		text: "I got your message!",
-	// 	}
-	// );
-
-	bot.start((ctx) =>
-		ctx.reply("Welcome to Hulugram Fund", {
-			reply_markup: {
-				inline_keyboard: [
-					[{ text: "Hulu Donate", web_app: { url: webLink } }],
-				],
-			},
-		})
+	await axios.post(
+		`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
+		{
+			chat_id: JSON.parse(event.body).message.chat.id,
+			text: "I got your message!",
+		}
 	);
-	bot.launch();
+
+	// bot.start((ctx) =>
+	// 	ctx.reply("Welcome to Hulugram Fund", {
+	// 		reply_markup: {
+	// 			inline_keyboard: [
+	// 				[{ text: "Hulu Donate", web_app: { url: webLink } }],
+	// 			],
+	// 		},
+	// 	})
+	// );
+	// bot.launch();
 	console.log("Received an update from Telegram After Open!", webLink);
 	return { statusCode: 200 };
 };
