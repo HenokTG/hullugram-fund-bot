b522c2b1661fe79573991bd8e5b04849f8f788bd
HenokTG <hena143877tg@gmail.com>
2022-07-28 12:20:46 +0300

    New Bot

--- netlify/functions/donate.js
+++ netlify/functions/donate.js
@@ -3,14 +3,58 @@ const { Telegraf } = require("telegraf");
 // require("dotenv").config();
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
-const webLink = process.env.APP_URL;
+const webLink =
+	"https://hulugram-fund.herokuapp.com/706699c0-0d8e-41bb-bdbe-a041833b1af7/donate";
 
-const bot = new Telegraf(BOT_TOKEN);
+const bot = new Telegraf("5524495934:AAFTNTejoN2bk2vSEr5IoK8KKJI12o3ZIgk");
 
-bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
+bot.start((ctx) => {
+	ctx.reply("Hello " + ctx.from.first_name + "!");
+	ctx.reply("Welcome to Hulugram Fund", {
+		reply_markup: {
+			inline_keyboard: [
+				[{ text: "Hulu Donate", web_app: { url: webLink } }],
+			],
+		},
+	});
+});
+bot.help((ctx) => {
+	ctx.reply("Send /start to receive a greeting");
+	ctx.reply("Send /keyboard to receive a message with a keyboard");
+	ctx.reply("Send /quit to stop the bot");
+});
+
 bot.on("sticker", (ctx) => ctx.reply("👍 good!"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there, Henok"));
+bot.hears("hi", (ctx) => ctx.reply(`Hey there, ${ctx.from.last_name}`));
+
+bot.command("quit", (ctx) => {
+	// Explicit usage
+	ctx.telegram.leaveChat(ctx.message.chat.id);
+	// Context shortcut
+	ctx.leaveChat();
+});
+
+bot.command("keyboard", (ctx) => {
+	ctx.reply("Keyboard", {
+		reply_markup: {
+			keyboard: [
+				[
+					{ text: "First option", callback: { data: "first" } },
+					{ text: "Second option", callback: { data: "second" } },
+				],
+			],
+		},
+	});
+});
+
+bot.on("text", (ctx) => {
+	ctx.reply(
+		"You choose the " +
+			(ctx.message.text === "First option" ? "First" : "Second") +
+			" Option!"
+	);
+});
+
 bot.launch();
 
 // Enable graceful stop
