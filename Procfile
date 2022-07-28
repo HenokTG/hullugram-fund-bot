bd1f171f42e0af982f7794ff0564466537996779
HenokTG <hena143877tg@gmail.com>
2022-07-28 12:52:18 +0300

    Procfile Changed

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: node netlify/functions/donate.js
--- netlify/functions/donate.js
+++ netlify/functions/donate.js
@@ -1,94 +1,62 @@
-// const axios = require("axios").default;
-
 const { Telegraf } = require("telegraf");
+
+// require("dotenv").config();
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const webLink = process.env.APP_URL;
 
 const bot = new Telegraf(BOT_TOKEN);
 
-exports.handler = async (event) => {
-	// const { Telegraf } = require("telegraf");
+bot.start((ctx) => {
+	ctx.reply("Hello " + ctx.from.first_name + "!");
+	ctx.reply("Welcome to Hulugram Fund", {
+		reply_markup: {
+			inline_keyboard: [
+				[{ text: "Hulu Donate", web_app: { url: webLink } }],
+			],
+		},
+	});
+});
+bot.help((ctx) => {
+	ctx.reply("Send /start to receive a greeting");
+	ctx.reply("Send /keyboard to receive a message with a keyboard");
+	ctx.reply("Send /quit to stop the bot");
+});
 
-	// require("dotenv").config();
+bot.on("sticker", (ctx) => ctx.reply("👍 good!"));
+bot.hears("hi", (ctx) => ctx.reply(`Hey there, ${ctx.from.last_name}`));
 
-	// const BOT_TOKEN = process.env.BOT_TOKEN;
-	// const webLink = process.env.APP_URL;
+bot.command("quit", (ctx) => {
+	// Explicit usage
+	ctx.telegram.leaveChat(ctx.message.chat.id);
+	// Context shortcut
+	ctx.leaveChat();
+});
 
-	// const bot = new Telegraf(BOT_TOKEN);
-
-	bot.start((ctx) => {
-		ctx.reply("Hello " + ctx.from.first_name + "!");
-		ctx.reply("Welcome to Hulugram Fund", {
-			reply_markup: {
-				inline_keyboard: [
-					[{ text: "Hulu Donate", web_app: { url: webLink } }],
+bot.command("keyboard", (ctx) => {
+	ctx.reply("Keyboard", {
+		reply_markup: {
+			keyboard: [
+				[
+					{ text: "First option", callback: { data: "first" } },
+					{ text: "Second option", callback: { data: "second" } },
 				],
-			},
-		});
+			],
+		},
 	});
-	bot.help((ctx) => {
-		ctx.reply("Send /start to receive a greeting");
-		ctx.reply("Send /keyboard to receive a message with a keyboard");
-		ctx.reply("Send /quit to stop the bot");
-	});
+});
 
-	bot.on("sticker", (ctx) => ctx.reply("👍 good!"));
-	bot.hears("hi", (ctx) => ctx.reply(`Hey there, ${ctx.from.last_name}`));
 
-	bot.command("quit", (ctx) => {
-		// Explicit usage
-		ctx.telegram.leaveChat(ctx.message.chat.id);
-		// Context shortcut
-		ctx.leaveChat();
-	});
+bot.on("text", (ctx) => {
+	ctx.reply(
+		"You choose the " +
+			(ctx.message.text === "First option" ? "First" : "Second") +
+			" Option!"
+	);
+});
 
-	bot.command("keyboard", (ctx) => {
-		ctx.reply("Keyboard", {
-			reply_markup: {
-				keyboard: [
-					[
-						{ text: "First option", callback: { data: "first" } },
-						{ text: "Second option", callback: { data: "second" } },
-					],
-				],
-			},
-		});
-	});
+bot.launch();
 
-	bot.on("text", (ctx) => {
-		ctx.reply(
-			"You choose the " +
-				(ctx.message.text === "First option" ? "First" : "Second") +
-				" Option!"
-		);
-	});
-
-	bot.launch();
-
-	// Enable graceful stop
-	process.once("SIGINT", () => bot.stop("SIGINT"));
-	process.once("SIGTERM", () => bot.stop("SIGTERM"));
-
-	return { statusCode: 200 };
-};
-
-// reply_markup: {
-// 	inline_keyboard: [
-// 		[{ text: "Hulu Donate", web_app: { url: webLink } }],
-// 	],
-// },
-
-// exports.handler = async (event) => {
-// 	console.log("Received an update from Telegram!", BOT_TOKEN, webLink);
-
-// await axios.post(
-// 	`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-// 	{
-// 		chat_id: JSON.parse(event.body).message.chat.id,
-// 		text: "I got your message!",
-// 	}
-// );
-
-//     return { statusCode: 200 };
-// };
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
