0d741b908c9bf18869b6fbab4194a60a4fca6259
HenokTG <hena143877tg@gmail.com>
2022-07-28 11:18:16 +0300

    snjnsdf

--- netlify/functions/donate.js
+++ netlify/functions/donate.js
@@ -1,19 +1,18 @@
-import { Telegraf } from "telegraf";
+const { Telegraf } = require("telegraf");
 
-require("dotenv").config();
+// require("dotenv").config();
 
-const BOT_TOKEN = process.env.TOKEN;
+const BOT_TOKEN = process.env.BOT_TOKEN;
 const webLink = process.env.APP_URL;
 
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on("sticker", (ctx) => ctx.reply("👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.on("sticker", (ctx) => ctx.reply("👍 good!"));
+bot.hears("hi", (ctx) => ctx.reply("Hey there, Henok"));
 bot.launch();
 
-
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
