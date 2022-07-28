9cd21a84b1e94adf4ce90629bafb6c1851379dfb
HenokTG <hena143877tg@gmail.com>
2022-07-28 13:01:36 +0300

    Some clean app

--- netlify/functions/donate.js
+++ netlify/functions/donate.js
@@ -1,14 +1,12 @@
 const { Telegraf } = require("telegraf");
 
-// require("dotenv").config();
-
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const webLink = process.env.APP_URL;
 
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.start((ctx) => {
-	ctx.reply("Hello " + ctx.from.first_name + "!");
+	ctx.reply("Hello, " + ctx.from.first_name + "!");
 	ctx.reply("Welcome to Hulugram Fund", {
 		reply_markup: {
 			inline_keyboard: [
@@ -16,43 +14,6 @@ bot.start((ctx) => {
 			],
 		},
 	});
-});
-bot.help((ctx) => {
-	ctx.reply("Send /start to receive a greeting");
-	ctx.reply("Send /keyboard to receive a message with a keyboard");
-	ctx.reply("Send /quit to stop the bot");
-});
-
-bot.on("sticker", (ctx) => ctx.reply("👍 good!"));
-bot.hears("hi", (ctx) => ctx.reply(`Hey there, ${ctx.from.last_name}`));
-
-bot.command("quit", (ctx) => {
-	// Explicit usage
-	ctx.telegram.leaveChat(ctx.message.chat.id);
-	// Context shortcut
-	ctx.leaveChat();
-});
-
-bot.command("keyboard", (ctx) => {
-	ctx.reply("Keyboard", {
-		reply_markup: {
-			keyboard: [
-				[
-					{ text: "First option", callback: { data: "first" } },
-					{ text: "Second option", callback: { data: "second" } },
-				],
-			],
-		},
-	});
-});
-
-
-bot.on("text", (ctx) => {
-	ctx.reply(
-		"You choose the " +
-			(ctx.message.text === "First option" ? "First" : "Second") +
-			" Option!"
-	);
 });
 
 bot.launch();
